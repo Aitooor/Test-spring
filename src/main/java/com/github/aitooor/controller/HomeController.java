afe062b62c43443e50090d97562b443d59fe4e17
Aitooor <aitorarias19@gmail.com>
2023-05-16 05:23:48 +0200

    fix: try

--- src/main/java/com/github/aitooor/controller/HomeController.java
+++ src/main/java/com/github/aitooor/controller/HomeController.java
@@ -2,13 +2,14 @@ package com.github.aitooor.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
     }
 }
